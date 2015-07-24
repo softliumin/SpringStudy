4507950684e796becc4e7dee308be84dcc015fb1
softliumin <softliumin@gmail.com>
2015-07-24 09:01:41 +0800

    first learning Spring

---
+++ src/main/java/cc/sharper/spring/SpringSharperStudy/App.java
@@ -0,0 +1,13 @@
+package cc.sharper.spring.SpringSharperStudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
